bf1cf2c88e58676a1bd78341754698bbfeb57efd
Vladislav Lewin <vlewin@macbook.fritz.box>
2016-02-14 11:57:29 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Vue + Materialize CSS (sass) application example
+
+## Components
+
+- Webpack
+- Materialize CSS (sass)
+- ESLint
+- JSCS
+- vue-loader
+- vue-router
+
+## Next
+
+- vue-validator: https://github.com/vuejs/vue-validator
+- vue-async-data: https://github.com/vuejs/vue-async-data
+- vuex: https://github.com/vuejs/vuex
+
+## Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# run unit tests
+npm test
+```
---
+++ build/webpack.base.config.js
@@ -0,0 +1,93 @@
+var webpack = require('webpack')
+var autoprefixer = require('autoprefixer')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var precss = require('precss')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './dist',
+    publicPath: 'dist/',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel!eslint',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+
+      {
+        test: /\.(scss|sass)$/,
+
+        // loader: ExtractTextPlugin.extract(
+        //   'style-loader', 'css-loader', 'postcss-loader', 'sass-loader'
+        // )
+
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          ['css-loader', 'postcss-loader', 'sass-loader']
+        )
+      },
+
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
+
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: '[name].[ext]?[hash:7]'
+        }
+      }
+    ]
+  },
+
+  vue: {
+    loaders: {
+      js: 'babel!eslint',
+
+      sass: 'style!css!sass?indentedSyntax',
+
+      // scss: 'style!css!sass',
+      // scss: ExtractTextPlugin.extract(
+      //   'style-loader', 'css-loader', 'sass-loader'
+      // ),
+
+      scss: 'style!css!sass',
+      autoprefixer: {
+        browsers: ['last 2 versions']
+      }
+    }
+
+  },
+
+  postcss: function () {
+    return [autoprefixer, precss]
+  },
+
+  eslint: {
+    formatter: require('eslint-friendly-formatter')
+  },
+
+  plugins: [
+    new ExtractTextPlugin('bundle.css'),
+
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Hammer: 'hammerjs/hammer'
+    })
+  ]
+}
